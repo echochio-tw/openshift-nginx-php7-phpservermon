3bdce4d2b8aa45cdc964b8982d40f47d049e91fe
nzgft <chio-nzgft>
2017-03-16 15:34:37 +0800

    update

--- template/www/index.php
+++ template/www/index.php
@@ -1,83 +1,33 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>Welcome to OpenShift</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css" />
-    <style type="text/css">
-        html, body {
-            margin: 0;
-            padding: 0;
-        }
+<?php
+/**
+ * PHP Server Monitor
+ * Monitor your servers and websites.
+ *
+ * This file is part of PHP Server Monitor.
+ * PHP Server Monitor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PHP Server Monitor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PHP Server Monitor.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package     phpservermon
+ * @author      Pepijn Over <pep@neanderthal-technology.com>
+ * @copyright   Copyright (c) 2008-2014 Pepijn Over <pep@neanderthal-technology.com>
+ * @license     http://www.gnu.org/licenses/gpl.txt GNU GPL v3
+ * @version     Release: v3.1.1
+ * @link        http://www.phpservermonitor.org/
+ **/
 
-        body {
-            font-family: 'Open Sans', sans-serif;
-            padding-top: 100px;
-        }
+require 'src/bootstrap.php';
 
-        h1 {
-            font-size: 52px;
-            margin-bottom: 0;
-        }
+psm_no_cache();
 
-        h1, h2, p {
-            margin-top: 0;
-            font-weight: normal;
-        }
-
-        a {
-            color: #DB242F;
-        }
-
-        a:not(:hover) {
-            text-decoration: none;
-        }
-
-        .container {
-            display: flex;
-            flex-direction: row;
-            margin: 0 auto;
-            width: 960px;
-        }
-
-        .section {
-            padding: 15px;
-        }
-
-        .section + .section {
-            margin-left: 15px;
-        }
-
-        .welcome {
-            background: #eee;
-            border-radius: 5px;
-            text-align: center;
-            flex: 0 0 300px;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <div class="section welcome">
-            <div>
-                <img src="/static/openshift-logo.png" alt="OpenShift Logo" />
-            </div>
-            <h1>Welcome!</h1>
-            <p>You are running PHP <?php echo phpversion(); ?></p>
-        </div>
-        <div class="section">
-            <h2>What's next?</h2>
-            <p>Make changes to this website by editing the files inside <code>www/</code> of your repository.</p>
-            <p>Commit your changes after, then push to the remote repository.</p>
-            <pre><code>$ git commit -a -m "Some commit message"
-$ git push origin master</code></pre>
-            <h2>More</h2>
-            <ul>
-                <li>Visit <a href="http://www.openshift.com">http://www.openshift.com</a>.</li>
-                <li>Get help in the <a href="http://www.openshift.com/forums/openshift">OpenShift forums</a>.</li>
-                <li>More on the cartridge at <a href="https://github.com/pinodex/openshift-cartridge-nginx-php7">pinodex/openshift-cartridge-nginx-php7</a>.</li>
-            </ul>
-        </div>
-    </div>
-</body>
-</html>
+$router = new psm\Router();
+$router->run();
